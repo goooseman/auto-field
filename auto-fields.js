8caa2696eca53f87cdb8bfec91c6db5a4df38ad9
goooseman <inbox@goooseman.ru>
2016-05-04 19:55:22 +0300

    [FIX] user password recovery wasnt working;

--- auto-fields.js
+++ auto-fields.js
@@ -9,8 +9,12 @@ Mongo.Collection.prototype.autoFields = function (fields) {
 
   collection.before.update(function (userId, doc, fieldNames, modifier) {
     modifier.$set = modifier.$set || {};
-    // Use minimongo package to simulate transform of the object
-    Package.minimongo.LocalCollection._modify(doc, modifier);
+    try {
+      // Use minimongo package to simulate transform of the object
+      Package.minimongo.LocalCollection._modify(doc, modifier);
+    } catch (err) {
+
+    }
     for (let functionName of Object.keys(fields)) {
       modifier.$set[functionName] = fields[functionName](doc, userId);
     }
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'goooseman:auto-fields',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Automaticly create collection fields on every save operation (insert/update). Like a before_save in RoR.',
   // URL to the Git repository containing the source code for this package.
